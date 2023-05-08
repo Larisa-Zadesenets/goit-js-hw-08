507a6ae95b2f31391357946e86c2428833a0c775
Larisa-Zadesenets <117529302+Larisa-Zadesenets@users.noreply.github.com>
2023-05-08 21:27:23 +0300

    дз2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,3 +4,20 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo(iframe);
 const localStorageTime = 'video-player-current-time';
+
+
+updateCornetTime();
+
+player.on('timeupdate', throttle(saveCornetTime, 1000));
+
+function saveCornetTime(data) {
+  localStorage.setItem(localStorageTime, data.seconds);
+}
+
+function updateCornetTime() {
+  const persistedData = localStorage.getItem(localStorageTime);
+
+  if (persistedData) {
+    player.setCurrentTime(localStorage.getItem(localStorageTime));
+  }
+}
